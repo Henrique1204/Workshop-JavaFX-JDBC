6e82c9816ddc9176381c7397b3d59cf94b31bedc
Paulo <pauloh16.pdr@gmail.com>
2020-05-29 01:03:45 -0300

    Implementar a View "DepartmentForm"

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerta;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entidades.Department;
 import model.servicos.DepartmentServico;
@@ -38,9 +47,11 @@ public class DepartmentListController implements Initializable
 
 	//Métodos implementados
 	@FXML
-	public void onBtnNovoAction()
+	public void onBtnNovoAction(ActionEvent evento)
 	{
-		System.out.println("OnBtnNovoAction");
+		Stage parentStage = Utils.stageAtual(evento);
+
+		ligarAoForm("/gui/DepartmentForm.fxml", parentStage);
 	}
 
 	private void iniciarNode()
@@ -62,6 +73,28 @@ public class DepartmentListController implements Initializable
 		List<Department> lista = servico.buscarTodos();
 		obsLista = FXCollections.observableArrayList(lista);
 		tableViewDepartment.setItems(obsLista);
+	}
+
+	private void ligarAoForm(String caminhoAbsoluto, Stage parentStage)
+	{
+		try
+		{
+			FXMLLoader carregar = new FXMLLoader(getClass().getResource(caminhoAbsoluto));
+			Pane pane = carregar.load();
+
+			Stage ligacaoStage = new Stage();
+			ligacaoStage.setTitle("Entre com os dados do Department");
+			ligacaoStage.setScene(new Scene(pane));
+			ligacaoStage.setResizable(false);
+			ligacaoStage.initOwner(parentStage);
+			ligacaoStage.initModality(Modality.WINDOW_MODAL);;
+			ligacaoStage.showAndWait();
+		}
+		catch (IOException e)
+		{
+			Alerta.mostrarAlerta("IO Exception", "Erro ao carregar a View", e.getMessage(), AlertType.ERROR);
+		}
+		
 	}
 
 	//Métodos da interface
